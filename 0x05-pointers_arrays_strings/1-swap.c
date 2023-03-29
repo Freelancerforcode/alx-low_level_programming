9f7dcbf4f78d58a87b71e84d85ade3069aacf1f5
Freelancerforcode <hirefreelancerforjob@gmail.com>
2023-03-29 05:28:42 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 /**
- * void swap_int(int *a, int *b);
+ * void - swap_int(int *a, int *b);
  * @a : Point integer to be swaped with a
  * @b : Point integer to be swaped with b
- * Retrun: 0
+ * Retrun: (void)
  */
 void swap_int(int *a, int *b);
 {
-       	int a = 98
-	int b = 42 
-	swap_int(&a,&b);
-	int c =*a;
+	int c = *a;
 	*b = *a;
 	*b = c;
-
-	return (0)
 }
