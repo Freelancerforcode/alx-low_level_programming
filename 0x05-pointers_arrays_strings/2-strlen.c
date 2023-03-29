7abfaae75e24ee20821546bb076cab54db61dc24
Freelancerforcode <hirefreelancerforjob@gmail.com>
2023-03-28 21:08:59 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - int _strlen(char *s);
+ * function:  that returns the length of a string.
+ * Return: 0
+ */
+int main(void)
+{
+	int _strlen(char *s);
+	int len;
+
+	len = strlen(*s);
+	_putchar("strlen (char *s): %d\n", *s);
+
+	return (0);
+}
