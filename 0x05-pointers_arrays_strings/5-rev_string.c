fb3076f4eabfb2f30ea32ec7606283d0e331e098
Freelancerforcode <hirefreelancerforjob@gmail.com>
2023-04-04 13:55:53 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,31 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- *@str: pointer to the string
+ * rev_string - prints a string in reverse
+ * @s: pointer to the string to be reversed
+ *
  * Return: void
  */
-void puts2(char *str)
-{
-	int i = 0;
 
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+void rev_string(char *s)
+{
+	int i = 0, j, right;
+
+	while (s[i])
 		i++;
+
+	j = i / 2;
+	right = 0;
+
+	while (right != j)
+	{
+		char temp = s[right];
+		int left = i - right - 1;
+
+		s[right] = s[left];
+		s[left] = temp;
+
+		right++;
 	}
 
-	_putchar('\n');
 }
